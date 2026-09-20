db45df4110bb30541e3135c53e6d64552ee3c24f
adv-Gyan <adv.gyanendrakr@gmail.com>
2026-09-21 03:10:31 +0530

    Fix catastrophic mass-replace bug that constrained elements to 120px, fixing all scaling/orientation issues for images and wavy line. Also implement a fixed hardware-accelerated background frame.

--- fix_wrapper.sh
+++ fix_wrapper.sh
@@ -2,14 +2,26 @@ sed -i '' '/\.mobile-wrapper {/,/z-index: 1;/c\
 .mobile-wrapper {\
   width: 100%;\
   max-width: 480px;\
-  background-color: var(--c-ivory);\
-  background-image: url('"'"'../assets/main_bg.png'"'"');\
-  background-size: 100% 100%;\
-  background-repeat: no-repeat;\
-  background-position: top center;\
+  background-color: transparent;\
   min-height: 100vh;\
   box-shadow: 0 0 40px rgba(0,0,0,0.1);\
   position: relative;\
   overflow-x: hidden;\
   z-index: 1;\
+}\
+\
+.bg-frame {\
+  position: fixed;\
+  top: 0;\
+  width: 100%;\
+  max-width: 480px;\
+  height: 100vh;\
+  background-image: url('"'"'../assets/main_bg.png'"'"');\
+  background-size: 100% 100%;\
+  background-position: center;\
+  background-repeat: no-repeat;\
+  z-index: -1;\
+  transform: translateZ(0);\
+  will-change: transform;\
+}\
 ' /Users/apple/Documents/Amar-Mansi-wedding/css/style.css
