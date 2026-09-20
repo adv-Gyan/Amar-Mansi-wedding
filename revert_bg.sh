ad7fed43be86aa4e891e7246c8528fd8e18ef7c3
adv-Gyan <adv.gyanendrakr@gmail.com>
2026-09-21 02:53:25 +0530

    Revert background to fixed cover (proportionate and seamless) as requested, fixing accidental stretch distortion and wrapper width

---
+++ revert_bg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sed -i '' 's/background-image: url('\''..\/assets\/main_bg.png'\'');//g' /Users/apple/Documents/Amar-Mansi-wedding/css/style.css
+sed -i '' 's/background-size: 100% 100%; \/\* Stretch completely to match full scroll height \*\///g' /Users/apple/Documents/Amar-Mansi-wedding/css/style.css
+sed -i '' 's/background-repeat: no-repeat;//g' /Users/apple/Documents/Amar-Mansi-wedding/css/style.css
+sed -i '' 's/background-position: top center;//g' /Users/apple/Documents/Amar-Mansi-wedding/css/style.css
